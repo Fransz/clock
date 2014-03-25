b13bbc789cda2f6fd3f92308e5a55590485ebba0
Frans <frans@vip.nl>
2014-03-26 00:43:58 +0100

    Ticking

--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@ paper.rect(10, 10, 810, 610).attr({fill: "black"});
 paper.customAttributes.hand = function(r, a, color) {
 
     var hsb = "hsb(".concat(color, ",", a / (2 * Math.PI) , ", .75)");
-    var large = a > Math.PI ? 1 : 0;
+    var large = a > Math.PI  && a < 2 * Math.PI ? 1 : 0;
     var ex = Math.cos(a) * r;
     var ey = -Math.sin(a) * r;
 
@@ -21,17 +21,55 @@ clock.push(dots(250, 60));
 clock.push(dots(200, 60));
 clock.push(dots(150, 60));
 
-var seconds = paper.path().attr({ hand: [250, 0 * Math.PI / 3, 0], "stroke-width": "20px"});
+// var seconds = paper.path().attr({ hand: [250, 0 * Math.PI / 3, 0], "stroke-width": "20px"});
+var seconds = {
+    r: 250,
+    hue: 0,
+    ticks: 60,
+    seconds: 0,
+
+    path: paper.path().attr({hand: [250, 0, 0], "stroke-width": "20px"}),
+
+    set: function(s) {
+        this.path.animate({hand: [this.r, 2 * Math.PI * s / this.ticks, this.hue]}, 600, "<");
+        this.seconds = s;
+    },
+
+    tick: function() {
+        this.seconds++;
+
+        var self = this;
+        var cb = function() {};
+        var effect = "elastic";
+
+        if(this.seconds === this.ticks) {
+            effect = "bounce";
+            cb = function() { self.path.attr({ hand: [this.r, 0, 0] }) };
+        }
+
+        this.path.animate({hand: [this.r, 2 * Math.PI * this.seconds / this.ticks, this.hue]}, 400, effect, cb);
+        if (this.seconds === this.ticks) this.seconds -= this.ticks;
+
+    }
+}
 var minutes = paper.path().attr({ hand: [200, 3 * Math.PI / 2, 1/3], "stroke-width": "20px"});
 var hours = paper.path().attr({ hand: [150, 3 * Math.PI / 2, 2/3], "stroke-width": "20px"});
 
-clock.push(seconds);
+clock.push(seconds.path);
 clock.push(minutes);
 clock.push(hours);
 
 clock.transform(transform);
 
-seconds.animate({hand: [250, 2 * Math.PI / 3, 0]}, 800, "<");
+// seconds.animate({hand: [250, 2 * Math.PI / 3, 0]}, 800, "<");
+var now = new Date();
+
+// seconds.set(now.getSeconds());
+seconds.set(57);
+function tick() {
+    seconds.tick();
+}
+setInterval(tick, 1000);
 
 function dots(r, cnt) {
     var dots = paper.set();
